48bfbb247bce9b93fa02ca5b09d986082ea638ef
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2024-07-03 04:36:28 +0800

    Update main.py
    Signed-off-by: Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>

--- main.py
+++ main.py
@@ -115,8 +115,8 @@ def send_email(subject, body, to, base64_video):
                 position: absolute;
                 top: 0;
                 left: 0;
-                width: 100%;
-                height: 100%;
+                width: 50%;
+                height: 50%;
             }}
         </style>
     </head>

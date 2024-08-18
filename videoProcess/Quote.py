53a670cedd2e43e7e6fd358aab04edace4de018c
Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>
2024-08-18 23:03:04 +0800

    Update Quote.py
    Signed-off-by: Edward Lance Lorilla <106719822+edwardlorilla1986@users.noreply.github.com>

--- videoProcess/Quote.py
+++ videoProcess/Quote.py
@@ -2,7 +2,7 @@ import requests
 import json
 import os
 from dotenv import load_dotenv
-
+import requests
 # load environment constants
 load_dotenv(".env")
 
@@ -11,9 +11,27 @@ load_dotenv(".env")
 # Format the response
 # Parse json  and return the single fact
 
+
+def format_message(poem):
+    poem_author = poem["author"]
+    poem_title = poem["title"]
+    poem_text = " "
+    for line in poem["lines"]:
+        poem_text += f"{line}\n"
+    return f'Poem "{poem_title}" by {poem_author} - {poem_text}'
+
+
+def fetch_poem(poet=""):
+    if poet:
+        url = f"https://poetrydb.org/author/{poet}/title,author,lines"
+    else:
+        url = "https://poetrydb.org/random/1/title,author,lines"
+
+    return requests.get(url).json()[0]
+
+
+
 def get_quote():
-    fact_key = os.environ["FACT_KEY"]
-    response = requests.request("GET", os.environ["FACT_URL"])
-    formatted = response.text
-    parse_json = json.loads(formatted)
-    return parse_json[fact_key]
+    random_poem = fetch_poem()
+    poem_message = format_message(random_poem)
+    return poem_message
